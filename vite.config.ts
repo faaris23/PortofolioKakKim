3fe5cf701d75a733b5198c77ab52f9ce22543bad
Faaris Sayyid Al-Ghozali <farisgozali23@gmail.com>
2026-07-11 18:46:41 +0700

    Konfigurasi Vite dan package.json untuk GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/rosalia-arts/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
